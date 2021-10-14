b9d7d7ec55eed031d4dbbc31bc6e001bdb4c6229
Martin <teszt1>
2021-10-14 13:05:38 +0200

    fényeroallitas

--- Encoder.ino
+++ Encoder.ino
@@ -1,9 +1,27 @@
+int SW=13;
+int outputCLK=11;
+int outputDT=10;
+int most=1;
+int ezelott=1;
+int szamlalo=0;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Encoder kezelő program!");
+  for(int i=11;i<=13;i++) pinMode(i, INPUT_PULLUP);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+  if(digitalRead(SW)==0) szamlalo=0;
+  
+  most=digitalRead(outputCLK);
+  if (most != ezelott);
+  {
+    if(digitalRead(outputDT) != most) { szamlalo++;}
+      else { szamlalo--;}
+      szamlalo=constrain(szamlalo, -10,10);
+    Serial.println(szamlalo);
+  }
+   ezelott=most;
+   }
